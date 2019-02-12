d5015271207ad90cdb900d49e2458c1a5780d0ea
OliverBurge <ollieontheweb@gmail.com>
2019-02-12 11:48:56 +0000

    Added edit functionality to comments

--- routes/comments.js
+++ routes/comments.js
@@ -36,7 +36,30 @@ router.post('/', isLoggedIn, function(req, res){
         }
     });
 });
-//middleware
+
+//EDIT
+router.get('/:comment_id/edit', function(req,res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.render('comments/edit', {campsite_id: req.params.id, comment: foundComment});           
+        }
+    });
+});
+
+//UPDATE 
+router.put('/:comment_id', function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect(`/campsites/${req.params.id}`);
+        }
+    })
+});
+
+//MIDDLEWARE
 function isLoggedIn(req,res,next){
     if (req.isAuthenticated()) {
         return next();
